640055a230a4c38d6636659452accc4908b5511e
Liu Yang <outcrop@163.com>
2020-06-10 10:48:04 +0800

    The second version of the spider with aex9 token support

---
+++ README.md
@@ -0,0 +1,1 @@
+The content of the bin directory could be exported from aeternity full node and aesohphia_cli.
---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+define('DATA_SRC_SITE', 'http://127.0.0.1:3013/');
+define('DB_USER', 'database user');
+define('DB_PASS', 'database password');
+//aeknow.db was pointed in /etc/hosts
